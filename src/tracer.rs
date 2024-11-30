2778eef16434c1137c94837f4f44ca7ca53428b7
Jaroslaw Nowosad <yarenty@gmail.com>
2024-11-30 09:54:10 +0000

    revoke changes on redirecting output to main stdin/stderr

--- src/tracer.rs
+++ src/tracer.rs
@@ -63,12 +63,17 @@ async fn main() -> Result<()> {
         let (path, app) = check_in_current_dir(app)?;
         info!("Application to be monitored is: {}, in dir {} , with params: {:?}", app, path, p);
 
+        //TODO: need to rethink
+        // cannot move output to stdin as it will destroy tui
+        // should go to some kind of buffered output
+        // or log to file
+        //TODO: Ctrl+C should be populated with kill command
         let cmd = Command::new(&path)
             .current_dir(get_current_working_dir())
             .args(p)
             .stdin(Stdio::null())
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
             // .stderr((Stdio::piped())
             .spawn()
             .expect("Failed to run ");
