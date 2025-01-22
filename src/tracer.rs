66af926905e26c206a5379c96639a54d647832a1
Jaroslaw Nowosad <yarenty@gmail.com>
2025-01-22 16:00:55 +0000

    added  output and err to file

--- src/tracer.rs
+++ src/tracer.rs
@@ -63,18 +63,15 @@ async fn main() -> Result<()> {
         let (path, app) = check_in_current_dir(app)?;
         info!("Application to be monitored is: {}, in dir {} , with params: {:?}", app, path, p);
 
-        //TODO: need to rethink
-        // cannot move output to stdin as it will destroy tui
-        // should go to some kind of buffered output
-        // or log to file
-        //TODO: Ctrl+C should be populated with kill command
+        let output_file = File::create(format!("{}.out", app))?;
+        let error_file = File::create(format!("{}.err", app))?;
+        
         let cmd = Command::new(&path)
             .current_dir(get_current_working_dir())
             .args(p)
             .stdin(Stdio::null())
-            .stdout(Stdio::null())
-            .stderr(Stdio::null())
-            // .stderr((Stdio::piped())
+            .stdout(Stdio::from(output_file))
+            .stderr(Stdio::from(error_file))
             .spawn()
             .expect("Failed to run ");
 
